1187fd3995704888ad298b3259059d879b39ee49
Joseph Cutler <jwc@seas.upenn.edu>
2024-12-03 20:38:47 -0500

    oops, rm explanation stuff

--- ocaml/lib/basicgen.ml
+++ ocaml/lib/basicgen.ml
@@ -21,28 +21,3 @@ let gen =
         )
       ]
   )
-
-(*
-let bind t ~f =
-  create (fun ~size ~random ->
-    let x = generate t ~size ~random in
-    generate (f x) ~size ~random)
-  
-*)
-
-let gen' =
-  fixed_point (
-    fun gt ->
-      let%bind n = size in
-      if n <= 1 then return Leaf else
-      weighted_union [
-        (1.0,return Leaf);
-        (Int.to_float n,
-          bind (with_size ~size:(n/2) gt) ~f:(fun l ->
-            bind (with_size ~size:(n/2) gt) ~f:(fun r ->
-              return (Node (l,r))
-            )
-          )
-        )
-      ]
-  )
