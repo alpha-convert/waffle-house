e8c503d5e336b8797da2a845d906564b20f086a4
Thia Richey <gannet@cs.washington.edu>
2025-01-27 16:15:33 -0500

    fast

--- staged-ocaml/lib/baseTypeStaged.ml
+++ staged-ocaml/lib/baseTypeStaged.ml
@@ -14,29 +14,26 @@ module BaseTypeStaged : Base_quickcheck.Test.S with type t = rbt = struct
            if size = 0 then
              E
            else
-             Base_quickcheck.Generator.generate (Base_quickcheck.Generator.create (fun ~size ~random ->
-              let float_value =
-                Splittable_random.float random ~lo:0. ~hi:0.2 in
-              if Float.(>=) float_value 0.1 then
-                Base_quickcheck.Generator.generate (Base_quickcheck.Generator.create
-                (fun ~size ~random ->
-                   let adjusted_size = Base.Int.pred size in
-                   T (
-                     (if Splittable_random.bool random then Impl.R else Impl.B),
-                     (Base_quickcheck.Generator.generate
-                        quickcheck_generator_tree
-                        ~size:adjusted_size
-                        ~random),
-                     ((Splittable_random.int random
-                         ~lo:Int.min_value ~hi:Int.max_value) mod 128),
-                     ((Splittable_random.int random
-                         ~lo:Int.min_value ~hi:Int.max_value) mod 128),
-                     (Base_quickcheck.Generator.generate
-                        quickcheck_generator_tree
-                        ~size:adjusted_size
-                        ~random)
-                   ))) ~size ~random
-              else
-                E)) ~size ~random)) in
+             let float_value = Splittable_random.float random ~lo:0. ~hi:0.2 in
+             let adjusted_size = Base.Int.pred size in
+             if Float.(>=) float_value 0.1 then
+               T (
+                 (if Splittable_random.bool random then Impl.R else Impl.B),
+                 (Base_quickcheck.Generator.generate
+                    quickcheck_generator_tree
+                    ~size:adjusted_size
+                    ~random),
+                 ((Splittable_random.int random
+                     ~lo:Int.min_value ~hi:Int.max_value) mod 128),
+                 ((Splittable_random.int random
+                     ~lo:Int.min_value ~hi:Int.max_value) mod 128),
+                 (Base_quickcheck.Generator.generate
+                    quickcheck_generator_tree
+                    ~size:adjusted_size
+                    ~random)
+               )
+             else
+               E))
+    in
     Base_quickcheck.Generator.of_lazy quickcheck_generator_tree
 end
