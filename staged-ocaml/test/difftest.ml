757c308632e1d130884f37965d31e63d7a817baa
Joseph Cutler <jwc@seas.upenn.edu>
2025-02-05 20:34:20 +0000

    work on difftest

---
+++ staged-ocaml/test/difftest.ml
@@ -0,0 +1,41 @@
+open Fast_gen;;
+open Core;;
+
+module type TestCase = sig
+  type t
+  val eq : t -> t -> bool
+  module F : functor (G : Generator_intf.GENERATOR) -> sig
+    val gen : t G.t
+  end
+end
+
+module SizeRand : Base_quickcheck.Test.S with type t = Int.t * Int.t = struct
+  type t = Int.t * Int.t [@@deriving quickcheck, sexp_of]
+
+  open Base_quickcheck.Generator;;
+  let quickcheck_generator =
+    bind small_strictly_positive_int ~f:(fun x ->
+      bind Base_quickcheck.Generator.int ~f:(fun y->
+        return (x,y)
+      )
+    )
+end
+
+module MakeDiffTest(T : TestCase) = struct
+  module BQ_G = T.F(Bq_generator)
+  module ST_G = T.F(Staged_generator)
+
+  let bq_gen = BQ_G.gen
+  let st_gen = Staged_generator.to_qc ST_G.gen
+
+  let run ?config () =
+    Base_quickcheck.Test.run_exn
+     ?config:config
+     ~f:(
+      fun (size,seed) ->
+        let v1 = Base_quickcheck.Generator.generate bq_gen ~size ~random:(Splittable_random.State.of_int seed) in
+        let v2 = Base_quickcheck.Generator.generate bq_gen ~size ~random:(Splittable_random.State.of_int seed) in
+        if T.eq v1 v2 then () else failwith "Failed!"
+     )
+     (module SizeRand)
+end
--- staged-ocaml/test/test_fast_gen.ml
+++ staged-ocaml/test/test_fast_gen.ml
@@ -1,13 +1,26 @@
-open Fast_gen.Codecps;;
-open Fast_gen.Codecps.Let_syntax;;
-module G = Fast_gen.Staged_generator;;
+open Fast_gen;;
+(* module G = Fast_gen.Staged_generator;; *)
 
-type tree = Empty | Node of tree * int * tree
+open Difftest
 
-let g = 
-  G.bind G.size ~f:(fun n ->
-    G.recursive .<(0,.~n)>. (
-      fun rcall lohi ->
-        _
+
+module Size_TC : TestCase = struct
+  type t = int
+  let eq x y = Int.equal x y
+  module F (G : Generator_intf.GENERATOR) = struct
+    let gen = 
+    G.bind G.size ~f:(fun nc ->
+      G.choose [
+        (G.lift 2., G.return 500);
+        (G.lift 1., G.return 1000);
+        (G.lift 3., G.return 100);
+      ]
     )
-  )
+  end
+end
+
+module DT = MakeDiffTest(Size_TC)
+
+let () = DT.run ()
+
+(* let () = G.print g *)
