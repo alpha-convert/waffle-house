21f6698a5fbcd6b6c6673acba0905cf4ac1b54e9
Ubuntu <ubuntu@ip-172-31-47-170.ec2.internal>
2025-03-05 23:43:52 +0000

    variant hell

---
+++ ppx_staged/bin/variant.mli
@@ -0,0 +1,12 @@
+open Stdio
+open Base
+open Ppx_staged
+open Fast_gen;;
+open Ppx_staged_expander;;
+
+type t = 
+| MyInt of int
+| MyFloat of (float * bool) * int
+| MyPair of int * float [@@deriving sexp]
+
+val show : t -> unit
