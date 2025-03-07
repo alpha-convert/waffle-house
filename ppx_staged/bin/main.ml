208fb984b4ef6220548fc8cca81974b24a76925e
Ubuntu <lapwing@seas.upenn.edu>
2025-03-07 00:34:20 +0000

    hmmmm

--- ppx_staged/bin/main.ml
+++ ppx_staged/bin/main.ml
@@ -10,6 +10,29 @@ open Sexplib0.Sexp_conv;;
 open Int;;
 open Base_quickcheck;;
 
+let quickcheck_generator_int =
+  Base_quickcheck.Generator.create (fun ~size:_ ~random ->
+    Splittable_random.int random ~lo:0 ~hi:10)
+
+type t = int [@@deriving wh, quickcheck, sexp]
+
+let () =
+  let st_generator = G_SR.jit ~extra_cmi_paths:["/home/ubuntu/waffle-house/ppx_staged/_build/default/bin/.main.eobjs/byte"] staged_quickcheck_generator in
+  let int_gen = quickcheck_generator_int in
+  let bq_generator = quickcheck_generator in
+  let () = G_SR.print staged_quickcheck_generator in 
+  let random = Splittable_random.State.of_int 11 in
+  let size = 3 in
+  for _ = 1 to 10 do 
+    printf "\n";
+    let st = Base_quickcheck.Generator.generate st_generator ~size ~random in
+    let bq = Base_quickcheck.Generator.generate bq_generator ~size ~random in
+    printf "========== Quickcheck generator ==========\n";
+    printf "%s\n" (Sexp.to_string_hum (sexp_of_t bq));
+    printf "========== Staged generator ==========\n";
+    printf "%s\n" (Sexp.to_string_hum (sexp_of_t st))
+  done
+(*
 let () =
   let generator = G_SR.jit ~extra_cmi_paths:["/home/ubuntu/waffle-house/ppx_staged/_build/default/bin/.main.eobjs/byte"] (My_tree_generator.staged_quickcheck_generator) in
   let () = G_SR.print (My_tree_generator.staged_quickcheck_generator) in  
@@ -24,3 +47,4 @@ let () =
     printf "========== Staged generator ==========\n";
     printf "%s\n" (Sexp.to_string_hum (My_tree_generator.sexp_of_t value2))
   done
+*)
