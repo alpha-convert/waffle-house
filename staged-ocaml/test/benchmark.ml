44ab6a029988af3ae1a2bfc1d9b5d49166af15f1
Joseph Cutler <jwc@seas.upenn.edu>
2025-03-09 00:49:13 +0000

    add support for magic-trace

--- staged-ocaml/test/benchmark.ml
+++ staged-ocaml/test/benchmark.ml
@@ -11,5 +11,9 @@ let bm ~bench_name ~named_gens ~(sizes : int list) ~num_calls ~seeds = Bench.ben
     Bench.Test.create_parameterised ~name:(bench_name ^ "_" ^ gen_name) ~args:(List.map ~f:(fun (sz,sd) -> ("n=" ^ Int.to_string sz ^ ",r=" ^ Int.to_string sd,(sz,sd))) @@ cartesian sizes seeds) @@
       fun (size,seed) ->
         let random = Splittable_random.State.of_int seed in
-        Staged.stage @@ fun () -> Base_quickcheck.Generator.generate g ~size:size ~random:random
+        let min_dur = Magic_trace.Min_duration.of_ns 3_000 in
+        Staged.stage @@ fun () ->
+          Magic_trace.mark_start ();
+          ignore (Base_quickcheck.Generator.generate g ~size:size ~random:random);
+          if Magic_trace.Min_duration.over min_dur then Magic_trace.take_snapshot ()
   )
