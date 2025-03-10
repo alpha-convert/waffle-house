d2ffbfb2858c07b39f342015d4f3b1945e153565
Joseph Cutler <jwc@seas.upenn.edu>
2025-03-10 15:29:42 +0000

    more perf testing

--- staged-ocaml/test/test_fast_gen.ml
+++ staged-ocaml/test/test_fast_gen.ml
@@ -325,15 +325,17 @@ module G_C_SR = Staged_generator.MakeStaged(C_sr_dropin_random)
 
 module Bm = Benchmark
 
-(* let () =
+let () =
   let module TC = IntList in
   let module M1 = TC.F(G_Bq) in
-  let module M2 = TC.F(G_C) in
-  let module M3 = TC.F(G_SR) in
+  let module M2 = TC.F(G_SR) in
+  let module M3 = TC.F(G_C) in
+  let module M4 = TC.F(G_C_SR) in
   let g1 = M1.gen in
-  let g2 = G_C.jit M2.gen in
-  let g3 = G_SR.jit M3.gen in
-  Benchmark.bm ~bench_name:"Int list" ~named_gens:["BQ",g1; "Staged C",g2; "Staged SR", g3] ~sizes:[10;50;100;1000] ~seeds:[100] ~num_calls:10000 *)
+  let g2 = G_SR.jit M2.gen in
+  let g3 = G_C.jit M3.gen in
+  let g4 = G_C_SR.jit M4.gen in
+  Benchmark.bm ~bench_name:"Int list" ~named_gens:["BQ",g1; "SR",g2; "C", g3; "CSR", g4] ~sizes:[10;50;100;1000] ~seeds:[100] ~num_calls:10000
 
 (* let () =
   let module TC = IntUIList in
@@ -386,7 +388,7 @@ let () =
   let g4 = G_C.jit M4.gen in
   Benchmark.bm ~bench_name:"Int list (uniform inclusive)" ~named_gens:["BQ",g1; "Staged SR",g2; "Staged CSR",g3; "Staged C", g4] ~sizes:[10;50;100;1000] ~seeds:[100] ~num_calls:10000
 
-(* let () =
+let () =
   let module TC = IntTC in
   let module M1 = TC.F(G_Bq) in
   let module M2 = TC.F(G_SR) in
@@ -396,7 +398,7 @@ let () =
   let g2 = G_SR.jit M2.gen in
   let g3 = G_C.jit M3.gen in
   let g4 = G_C_SR.jit M4.gen in
-  Benchmark.bm ~bench_name:"int" ~named_gens:["BQ",g1; "Staged SR",g2; "Staged C", g3; "Staged CSR", g4] ~sizes:[10;50;100;1000] ~seeds:[100] ~num_calls:100000 *)
+  Benchmark.bm ~bench_name:"int" ~named_gens:["BQ",g1; "Staged SR",g2; "Staged C", g3; "Staged CSR", g4] ~sizes:[10;50;100;1000] ~seeds:[100] ~num_calls:100000
 
 
 let path = "/home/ubuntu/waffle-house/staged-ocaml/_build/default/test/.test_fast_gen.eobjs/byte/"
