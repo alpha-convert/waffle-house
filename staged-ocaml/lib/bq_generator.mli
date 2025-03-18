8c178876af048a74a594c438ab497ac1f43a6d93
Joseph Cutler <jwc@seas.upenn.edu>
2025-03-18 18:14:11 +0000

    add bind counter

--- staged-ocaml/lib/bq_generator.mli
+++ staged-ocaml/lib/bq_generator.mli
@@ -1,1 +1,4 @@
 include Generator_intf.S with type 'a t = 'a Base_quickcheck.Generator.t with type 'a C.t = 'a with module R = Sr_random
+
+val get_num_binds : unit -> int
+val reset_bind_count : unit -> unit
